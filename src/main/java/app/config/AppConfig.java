12526b97b89edfbc840802f137a24e3108f162fe
MDP87 <hoplalay@mail.ru>
2023-01-21 21:54:18 +0300

    done

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,37 @@
 package app.config;
 
+import app.model.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+    @Bean (name = "cat")
+    public Cat getCat() {
+        return new Cat();
+    }
+    @Bean (name = "dog")
+    public Dog getDog() {
+        return new Dog();
+    }
+    @Bean (name = "animalsCage")
+    public AnimalsCage getAnimalsCage() {
+        return new AnimalsCage();
+    }
+
+    @Bean (name = "timer")
+    public Timer getTimer() {
+        return new Timer();
+    }
+
+
+
 }
